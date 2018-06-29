905204963b87d999bebfadb228b447e7546442d7
Anthony Fodor <anthony.fodor@gmail.com>
2018-06-29 02:30:56 -0400

    progress bars...

--- tdemos/src/App.js
+++ tdemos/src/App.js
@@ -20,9 +20,31 @@ class App extends Component
 				    accessor: 'jobName' 
 				  },
 				  {
-				    Header: 'progress',
-				    accessor: 'progress' 
-				  }  
+				        Header: 'progress',
+				        accessor: 'progress',
+				        Cell: row => (
+				          <div
+				            style={{
+				              width: '100%',
+				              height: '100%',
+				              backgroundColor: '#dadada',
+				              borderRadius: '2px'
+				            }}
+				          >
+				            <div
+				              style={{
+				                width: `${row.value}%`,
+				                height: '100%',
+				                backgroundColor: row.value > 66 ? '#85cc00'
+				                  : row.value > 33 ? '#ffbf00'
+				                  : '#ff2e00',
+				                borderRadius: '2px',
+				                transition: 'all .2s ease-out'
+				              }}
+				            />
+				          </div>
+				        )
+				      }
 				 ]
 				,
 				list : this.makeList(100)
@@ -32,7 +54,7 @@ class App extends Component
   componentDidMount()
   {
 	  console.log(this)
-	  this.timerID = setInterval(() => this.changeProgress() , 100);
+	  this.timerID = setInterval(() => this.changeProgress() , 10);
   }
   
   componentWillUnmount()
