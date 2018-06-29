8f99f1386667ed48cfffb631c32d140b56c5691c
Anthony Fodor <anthony.fodor@gmail.com>
2018-06-29 01:32:28 -0400

    add 5 simulated nodes...

--- tdemos/src/App.js
+++ tdemos/src/App.js
@@ -2,20 +2,69 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import 'react-table/react-table.css'
+import ReactTable from "react-table";
+
+const columns = [{
+    Header: 'node',
+    accessor: 'node'
+  },{
+    Header: 'jobName',
+    accessor: 'jobName' 
+  },
+  {
+    Header: 'progress',
+    accessor: 'progress' 
+  }  
+ ]
+
+const list = [
+              {
+              	node: "Node 1",
+              	jobName : "RDP 1",
+              	progress : 20,
+              	id : 1
+              },
+              {
+            	  node: "Node 2",
+                	jobName : "RDP 2",
+                	progress : 3,
+                	id : 2
+                }
+              ,
+              {
+            	  node: "Node 3",
+                	jobName : "RDP 3",
+                	progress : 20,
+                	id : 3
+                },
+                {
+              	  node: "Node 4",
+                  	jobName : "RDP 4",
+                  	progress : 20,
+                  	id : 4
+                  },
+                  {
+                	  node: "Node 4",
+                    	jobName : "RDP 4",
+                    	progress : 20,
+                    	id : 5
+                    }
+              ];
+
+
+class App extends Component 
+{
+
+  render() 
+    {
+      return(
+  	 <ReactTable 
+      data={list}
+      columns={columns}
+
+     />)
+  } 
 }
 
 export default App;
