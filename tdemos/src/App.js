24adc9346464d3156ef6b35daa01e8dad7d40d39
Anthony Fodor <anthony.fodor@gmail.com>
2018-06-29 01:39:50 -0400

    data is now part of state...

--- tdemos/src/App.js
+++ tdemos/src/App.js
@@ -5,64 +5,74 @@ import './App.css';
 import 'react-table/react-table.css'
 import ReactTable from "react-table";
 
-const columns = [{
-    Header: 'node',
-    accessor: 'node'
-  },{
-    Header: 'jobName',
-    accessor: 'jobName' 
-  },
-  {
-    Header: 'progress',
-    accessor: 'progress' 
-  }  
- ]
-
-const list = [
-              {
-              	node: "Node 1",
-              	jobName : "RDP 1",
-              	progress : 20,
-              	id : 1
-              },
-              {
-            	  node: "Node 2",
-                	jobName : "RDP 2",
-                	progress : 3,
-                	id : 2
-                }
-              ,
-              {
-            	  node: "Node 3",
-                	jobName : "RDP 3",
-                	progress : 20,
-                	id : 3
-                },
-                {
-              	  node: "Node 4",
-                  	jobName : "RDP 4",
-                  	progress : 20,
-                  	id : 4
-                  },
-                  {
-                	  node: "Node 4",
-                    	jobName : "RDP 4",
-                    	progress : 20,
-                    	id : 5
-                    }
-              ];
-
 
 class App extends Component 
 {
 
+	constructor(props)
+	{
+		super(props);
+		
+		this.state = 
+		{
+				columns : [{
+				    Header: 'node',
+				    accessor: 'node'
+				  },{
+				    Header: 'jobName',
+				    accessor: 'jobName' 
+				  },
+				  {
+				    Header: 'progress',
+				    accessor: 'progress' 
+				  }  
+				 ]
+				,
+				list : [
+			              {
+			              	node: "Node 1",
+			              	jobName : "RDP 1",
+			              	progress : 20,
+			              	id : 1
+			              },
+			              {
+			            	  node: "Node 2",
+			                	jobName : "RDP 2",
+			                	progress : 3,
+			                	id : 2
+			                }
+			              ,
+			              {
+			            	  node: "Node 3",
+			                	jobName : "RDP 3",
+			                	progress : 20,
+			                	id : 3
+			                },
+			                {
+			              	  node: "Node 4",
+			                  	jobName : "RDP 4",
+			                  	progress : 20,
+			                  	id : 4
+			                  },
+			                  {
+			                	  node: "Node 4",
+			                    	jobName : "RDP 4",
+			                    	progress : 20,
+			                    	id : 5
+			                    }
+			              ]
+		}
+		
+	}
+
+	
+	
   render() 
     {
       return(
   	 <ReactTable 
-      data={list}
-      columns={columns}
-
+      data={this.state.list}
+      columns={this.state.columns}
      />)
   } 
 }
